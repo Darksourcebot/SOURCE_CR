17e039c252f32e07ed42e9390dcc2046360d3a32
devmonzer0 <150098012+devmonzer0@users.noreply.github.com>
2023-11-09 03:04:06 +0200

    Update devwelcome.py

--- AnonX/plugins/devwelcome.py
+++ AnonX/plugins/devwelcome.py
@@ -1,6 +1,5 @@
 from pyrogram import Client
 from pyrogram.types import ChatMemberUpdated, InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.enums import ChatMemberStatus
 from Anonx import app
 import os
 
@@ -8,7 +7,7 @@ import os
 @app.on_chat_member_updated(filters=lambda _, response: response.new_chat_member)
 async def WelcomeDev(_, response: ChatMemberUpdated):
     dev_id = 6085292025 # حط ايديك هنا
-    if response.from_user.id == dev_id and response.new_chat_member.status == ChatMemberStatus.MEMBER:
+    if response.from_user.id == dev_id:
         info = await app.get_chat(dev_id)
         name = info.first_name
         bio = info.bio
