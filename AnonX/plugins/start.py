6b27c38fdeb0cad5192c6883689a237c981c7a20
DEV-E3DAM <139201770+Darksourcebot@users.noreply.github.com>
2023-11-24 09:29:36 +0200

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -174,7 +174,7 @@ async def start_comm(client, message: Message, _):
                             text="• ʏᴏᴜᴛᴜʙᴇ •", url=f"{link}"
                         ),
                         InlineKeyboardButton(
-                            text="𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 •", url="https://t.me/pp_g3"
+                            text="𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 •", url="https://t.me/SOURCE_CR"
                         ),
                     ],
                 ]
